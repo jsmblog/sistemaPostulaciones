0da9a438ca78e55aa88e4a6299580893de1856ab
jsmblog <jsmblog19@gmail.com>
2025-11-07 15:25:16 -0500

    commit 12

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { supabase } from "../supabase/connection";
-import { insertUserData } from "../services/insert";
+
 import { useUser } from "../context/UserContext";
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
